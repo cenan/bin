4e1f01f6a32a46e3c8f0442d08d2fda030cf1cde
cenan <me@cenanozen.com>
2013-04-12 16:26:07 +0300

    added tmux_attach_if_exists function to common.sh

--- common.sh
+++ common.sh
@@ -2,6 +2,16 @@
 # this file should always be named as
 # $HOME/bin/common.sh
 
+function tmux_attach_if_exists {
+	tmux has-session -t $1
+	if [ $? -eq 0 ]; then
+		echo "Session $1 already exists, attaching..."
+		sleep 1
+		tmux attach -t $1
+		exit 0;
+	fi
+}
+
 txtred=$(tput setaf 1)
 txtgreen=$(tput setaf 2)
 txtyellow=$(tput setaf 3)
